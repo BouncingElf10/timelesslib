586a596fd2eea4c42fa25fc3e32d48a065364780
BouncingElf10 <mathisbonne09@gmail.com>
2025-12-08 23:07:35 +0100

    changed CountdownManager to ClientCountdownManager in TimelessLibClient

--- src/main/java/dev/bouncingelf10/timelesslib/TimelessLibClient.java
+++ src/main/java/dev/bouncingelf10/timelesslib/TimelessLibClient.java
@@ -3,7 +3,7 @@ package dev.bouncingelf10.timelesslib;
 import dev.bouncingelf10.timelesslib.api.animation.AnimationManager;
 import dev.bouncingelf10.timelesslib.api.clock.TimelessClock;
 import dev.bouncingelf10.timelesslib.api.cooldown.ClientCooldownManager;
-import dev.bouncingelf10.timelesslib.api.countdown.CountdownManager;
+import dev.bouncingelf10.timelesslib.api.countdown.ClientCountdownManager;
 import dev.bouncingelf10.timelesslib.api.scheduler.Scheduler;
 import dev.bouncingelf10.timelesslib.fabric.TimelessFabricHelper;
 import net.fabricmc.api.ClientModInitializer;
@@ -16,7 +16,7 @@ import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class TimelessLibClient implements ClientModInitializer {
 	public static final Scheduler<Minecraft> clientScheduler = new Scheduler<>(TimelessFabricHelper::getClient);
-	public static final CountdownManager<Minecraft> clientCountdownManager = new CountdownManager<>(TimelessFabricHelper::getClient);
+	public static final ClientCountdownManager<Minecraft> clientCountdownManager = new ClientCountdownManager<>(TimelessFabricHelper::getClient);
 	public static final ClientCooldownManager<Minecraft> clientCooldownManager = new ClientCooldownManager<>(TimelessFabricHelper::getClient, UUID.randomUUID());
     public static final AnimationManager clientAnimationManager = new AnimationManager();
 
@@ -31,7 +31,7 @@ public class TimelessLibClient implements ClientModInitializer {
 	public static Scheduler<Minecraft> getClientScheduler() {
 		return clientScheduler;
 	}
-	public static CountdownManager<Minecraft> getClientCountdownManager() {
+	public static ClientCountdownManager<Minecraft> getClientCountdownManager() {
 		return clientCountdownManager;
 	}
 	public static ClientCooldownManager<Minecraft> getClientCooldownManager() {
